4e8790e65df4c5f6b7f3f0aad704c9a5d1657238
haskeyvas <vasileioskritikos@aueb.gr>
2024-06-02 10:33:20 -0700

    hello2 tested and changed

--- src/gr/aueb/cf/Hello2.java
+++ src/gr/aueb/cf/Hello2.java
@@ -3,6 +3,6 @@ package gr.aueb.cf;
 public class Hello2 {
 
     public static void main(String[] args) {
-        System.out.println("hello again!");
+        System.out.println("hello again and again!!");
     }
 }
